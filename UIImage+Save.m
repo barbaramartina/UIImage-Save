415eda22110baaebe26fd19e6ef8e7eb84dfc3c3
Barbaramartina <barbararodeker@gmail.com>
2014-04-21 15:38:36 -0300

    Category to save images to photos album

---
+++ UIImage+Save.m
@@ -0,0 +1,76 @@
+//
+//  UIImage+Save.m
+//  ProWatermarkMaker
+//
+//  Created by Barbara Rodeker on 4/21/14.
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+
+#import "UIImage+Save.h"
+#import <objc/runtime.h>
+
+static char const * const kSuccessBlockKey = "UIImageOnSaveSuccessBlock";
+static char const * const kErrorBlockKey = "UIImageOnSaveErrorBlock";
+
+@implementation UIImage (Save)
+
+@dynamic successBlock;
+@dynamic errorBlock;
+
+- (void)saveToPhotosAlbumOnSuccess:(UIImageOnSaveSuccessBlock)successBlock
+                           onError:(UIImageOnSaveErrorBlock)errorBlock
+{
+    self.successBlock = successBlock;
+    self.errorBlock = errorBlock;
+    
+    UIImageWriteToSavedPhotosAlbum(self, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+}
+
+- (UIImageOnSaveSuccessBlock)successBlock {
+    return objc_getAssociatedObject(self, kSuccessBlockKey);
+}
+
+- (void)setSuccessBlock:(UIImageOnSaveSuccessBlock)successBlockNew {
+    objc_setAssociatedObject(self, kSuccessBlockKey, successBlockNew, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UIImageOnSaveErrorBlock)errorBlock {
+    return objc_getAssociatedObject(self, kSuccessBlockKey);
+}
+
+- (void)setErrorBlock:(UIImageOnSaveErrorBlock)errorBlockNew {
+    objc_setAssociatedObject(self, kErrorBlockKey, errorBlockNew, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+#pragma mark Private 
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    if (error) {
+        if (self.errorBlock) {
+            self.errorBlock(error);
+        }
+    } else {
+        if (self.successBlock) {
+            self.successBlock();
+        }
+    }
+}
+
+
+@end
